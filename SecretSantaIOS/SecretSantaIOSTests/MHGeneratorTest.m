5e982c749374d0eaad991e72cee6c17f0fbdc033
micahhainline <micah.hainline@gmail.com>
2012-08-14 10:10:20 -0500

    Added final tests.

--- SecretSantaIOS/SecretSantaIOSTests/MHGeneratorTest.m
+++ SecretSantaIOS/SecretSantaIOSTests/MHGeneratorTest.m
@@ -54,7 +54,76 @@
     STAssertFalse([self assignmentsContainSamePerson: assignments], nil);
 }
 
+- (void)testWhenPeopleWithTheSameNameAreAddedThenAssignmentsDoNotContainFamilyMembers {
+    MHPerson *person1 = [[MHPerson alloc] initWithFirst: @"Adam" andLastName: @"Arlington"];
+    MHPerson *person2 = [[MHPerson alloc] initWithFirst: @"Bob" andLastName: @"Arlington"];
+    MHPerson *person3 = [[MHPerson alloc] initWithFirst: @"Askara" andLastName: @"Barnes"];
+    MHPerson *person4 = [[MHPerson alloc] initWithFirst: @"Billy" andLastName: @"Barnes"];
+    MHPerson *person5 = [[MHPerson alloc] initWithFirst: @"Aaron" andLastName: @"Chung"];
+    
+    MHGenerator *testObject = [[MHGenerator alloc] init];
+    
+    NSArray *people = @[person1, person2, person3, person4, person5];
+    NSArray *assignments = [testObject createAssignmentsForPeople: people];
+    NSArray *givers = [self giversFromAssignments:assignments];
+    STAssertEqualObjects(givers, people, nil);
+    NSArray *recipients = [self recipientsFromAssignments:assignments];
+    STAssertTrue([recipients containsObject: person1], nil);
+    STAssertTrue([recipients containsObject: person2], nil);
+    STAssertTrue([recipients containsObject: person3], nil);
+    STAssertTrue([recipients containsObject: person4], nil);
+    STAssertTrue([recipients containsObject: person5], nil);
+    STAssertFalse([self assignmentsContainSamePerson: assignments], nil);
+    STAssertFalse([self assignmentsContainTwoPeopleFromTheSameFamily: assignments], nil);
+}
 
+- (void)testWhenOnlyOnePersonExistsThenNilIsReturned {
+    MHPerson *person1 = [[MHPerson alloc] initWithFirst: @"Adam" andLastName: @"Arlington"];
+       
+    MHGenerator *testObject = [[MHGenerator alloc] init];
+
+    NSArray *people = @[person1];
+    NSArray *assignments = [testObject createAssignmentsForPeople: people];
+    
+    STAssertNil(assignments, nil);
+}
+
+- (void)testWhenNoMatchesExistBecauseOfTooManyOfOneFamilyThenNilIsReturned {
+    MHPerson *person1 = [[MHPerson alloc] initWithFirst: @"Adam" andLastName: @"Arlington"];
+    MHPerson *person2 = [[MHPerson alloc] initWithFirst: @"Bob" andLastName: @"Arlington"];
+    MHPerson *person3 = [[MHPerson alloc] initWithFirst: @"Charlie" andLastName: @"Arlington"];
+    MHPerson *person4 = [[MHPerson alloc] initWithFirst: @"Alice" andLastName: @"Bones"];
+    MHPerson *person5 = [[MHPerson alloc] initWithFirst: @"Betty" andLastName: @"Bones"];
+    
+    MHGenerator *testObject = [[MHGenerator alloc] init];
+    
+    NSArray *people = @[person1, person2, person3, person4, person5];
+    NSArray *assignments = [testObject createAssignmentsForPeople: people];
+    
+    STAssertNil(assignments, nil);
+}
+
+- (void)testAllMatchesCanBeCreated {
+    MHPerson *person1 = [[MHPerson alloc] initWithFirst: @"Adam" andLastName: @"Arlington"];
+    MHPerson *person2 = [[MHPerson alloc] initWithFirst: @"Bob" andLastName: @"Arlington"];
+    MHPerson *person3 = [[MHPerson alloc] initWithFirst: @"Askara" andLastName: @"Barnes"];
+    MHPerson *person4 = [[MHPerson alloc] initWithFirst: @"Billy" andLastName: @"Barnes"];
+    MHPerson *person5 = [[MHPerson alloc] initWithFirst: @"Aaron" andLastName: @"Chung"];
+    
+    MHGenerator *testObject = [[MHGenerator alloc] init];
+    
+    NSArray *people = @[person1, person2, person3, person4, person5];
+    NSArray *expectedRecipients = @[person5, person3, person2, person1, person4];
+    BOOL found = NO;
+    int count = 0;
+    while (count < 20000 && !found) {
+        count++;
+        NSArray *assignments = [testObject createAssignmentsForPeople: people];
+        NSArray *recipients = [self recipientsFromAssignments:assignments];
+        found = [recipients isEqualToArray:expectedRecipients];
+    }
+    STAssertTrue(found, nil);
+}
 
 - (NSArray *)giversFromAssignments: (NSArray *)assignments {
     NSMutableArray *givers = [NSMutableArray array];
